3d796a5d3fa64601da670c151a80e1ed26409406
Attila Kerekes <k.attila@gmail.com>
2012-03-10 12:15:49 +0100

    jQuery Plugin please

---
+++ js/jquery.rgbpwd.js
@@ -0,0 +1,130 @@
+/*!
+ *
+ * RGB Password jQuery Plugin
+ * @author: Attila Kerekes
+ * @date: 2012.03.10.
+ *
+ */
+
+if ( typeof Object.create !== 'function' ) {
+    Object.create = function( obj ) {
+        function F() {}
+        F.prototype = obj;
+        return new F();
+    };
+}
+
+(function ($, window, document, undefined) {
+
+    var RGBPassword = {
+        init: function ( options, elem ) {
+            var self = this;
+            self.elem = elem;
+            self.$elem = $( elem );
+
+            self.options = $.extend( {}, $.fn.rgbpassword.options, options );
+
+            self.colorBoxes = [
+                $('<div class="color1"></div>').appendTo(this.options.colorBoxContainer),
+                $('<div class="color2"></div>').appendTo(this.options.colorBoxContainer),
+                $('<div class="color3"></div>').appendTo(this.options.colorBoxContainer)
+            ];
+
+            self.bindEvent();
+            self.adjustColor('');
+        },
+
+        bindEvent: function() {
+            var self = this;
+
+            self.$elem.on('keyup', function() {
+                self.adjustColor(self.elem.value);
+            });
+        },
+
+        adjustColor: function(value) {
+            for(var i in this.colorBoxes) {
+
+                var box  = this.colorBoxes[i],
+                    salt = this.options.salts[i],
+                    hue  = this.jenkins_hash(value, salt)/salt,
+                    color;
+
+                color = (value.length < 4)
+                    ? color = this.hslToRgb(0, 0, hue)
+                    : color = this.hslToRgb(hue, this.options.saturation, this.options.lightness);
+
+                box.css({
+                    backgroundColor: "rgb(" + color.r + ", " + color.g + ", " + color.b + ")"
+                });
+            }
+        },
+
+        jenkins_hash: function(key, interval_size) {
+            var hash = 0;
+            for (var i=0; i<key.length; ++i) {
+                hash += key.charCodeAt(i);
+                hash += (hash << 10);
+                hash ^= (hash >> 6);
+            }
+            hash += (hash << 3);
+            hash ^= (hash >> 11);
+            hash += (hash << 15);
+            // make unsigned and modulo interval_size
+            return (hash >>> 0) % interval_size;
+        },
+
+        hslToRgb: function hslToRgb(h, s, l){
+            var r, g, b;
+
+            if (s == 0){
+                r = g = b = l; // achromatic
+            } else {
+                var q = l < 0.5 ? l * (1 + s) : l + s - l * s,
+                    p = 2 * l - q;
+
+                r = this.hue2rgb(p, q, h + 1/3);
+                g = this.hue2rgb(p, q, h);
+                b = this.hue2rgb(p, q, h - 1/3);
+            }
+
+            return {
+                r : this.finalise(r),
+                g : this.finalise(g),
+                b : this.finalise(b)
+            };
+        },
+
+        hue2rgb: function(p, q, t){
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6) return p + (q - p) * 6 * t;
+            if(t < 1/2) return q;
+            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        },
+
+        finalise: function(color) {
+            return Math.round(color * 255)
+        }
+    };
+
+    $.fn.rgbpassword = function ( options ) {
+        return this.each(function () {
+
+            var rgbpw = Object.create( RGBPassword );
+            rgbpw.init( options, this );
+
+            // Make the instance available
+            $.data( this, 'RGBPassword', rgbpw );
+        });
+    };
+
+    $.fn.rgbpassword.options = {
+        salts: [ 2462, 3637, 7432],
+        colorBoxContainer: '.colorbox',
+        saturation: 0.5,
+        lightness: 0.5
+    };
+
+})(jQuery, window, document);
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
-JavaScript RGB Password
-=======================
+jQuery RGB Password Plugin
+==========================
 
 Generates a simple RGB pattern based on the password input. The idea is that the input is recognisable by the user so that they understand their password to be entered correctly before submitting.
 
-Demo:
-[skattyadz.github.com/JavaScript-RGB-Password/index.html](http://skattyadz.github.com/JavaScript-RGB-Password/index.html)
-
 Idea based on a Twitter conversation between @jamesrwhite and @skattyadz
+
+// http://findingscience.com/javascript/hashing/memcache/2010/12/28/javascript-implementation-of-jenkins-hash.html
+
+// Based on http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+
+jQuery Plugin by @keriati
